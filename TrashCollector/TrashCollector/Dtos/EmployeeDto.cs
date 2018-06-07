9c6de4d8bab50862c5f6dae414f9c8b62d213d01
kristinschramm <kristin.schramm@gmail.com>
2018-06-07 10:33:57 -0500

    add employee dto

---
+++ TrashCollector/TrashCollector/Dtos/EmployeeDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrashCollector.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrashCollector.Dtos
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [Range(11111, 99999)]
+        public int ZipCode { get; set; }
+        [Phone]
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
